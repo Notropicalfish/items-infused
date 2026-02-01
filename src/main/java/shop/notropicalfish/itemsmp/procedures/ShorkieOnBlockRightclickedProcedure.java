5fb4c54c59e783b3513b77c313e02ac946d75026
Notropicalfish <itoasted319@gmail.com>
2026-01-31 22:08:21 -0500

    I did a thing but liek it doesent work :3

--- src/main/java/shop/notropicalfish/itemsmp/procedures/ShorkieOnBlockRightclickedProcedure.java
+++ src/main/java/shop/notropicalfish/itemsmp/procedures/ShorkieOnBlockRightclickedProcedure.java
@@ -2,6 +2,8 @@ package shop.notropicalfish.itemsmp.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.Mth;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
@@ -11,13 +13,41 @@ import net.minecraft.core.BlockPos;
 
 public class ShorkieOnBlockRightclickedProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
+		double sqeek = 0;
 		if (world instanceof ServerLevel _level)
 			_level.sendParticles(ParticleTypes.HEART, x, y, z, 5, 0.3, 0.3, 0.3, 1);
-		if (world instanceof Level _level) {
-			if (!_level.isClientSide()) {
-				_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("entity.rabbit.death")), SoundSource.MASTER, 1, 1);
-			} else {
-				_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("entity.rabbit.death")), SoundSource.MASTER, 1, 1, false);
+		sqeek = Mth.nextInt(RandomSource.create(), 1, 4);
+		if (sqeek == 1) {
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("itemsmp:shorkiesqueek")), SoundSource.MASTER, 1, 1);
+				} else {
+					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("itemsmp:shorkiesqueek")), SoundSource.MASTER, 1, 1, false);
+				}
+			}
+		} else if (sqeek == 2) {
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("itemsmp:shorkiesqueek")), SoundSource.MASTER, 1, 1);
+				} else {
+					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("itemsmp:shorkiesqueek")), SoundSource.MASTER, 1, 1, false);
+				}
+			}
+		} else if (sqeek == 3) {
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("itemsmp:shorkiesqueek")), SoundSource.MASTER, 1, 1);
+				} else {
+					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("itemsmp:shorkiesqueek")), SoundSource.MASTER, 1, 1, false);
+				}
+			}
+		} else if (sqeek == 4) {
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("itemsmp:shorkiesqueek")), SoundSource.MASTER, 1, 1);
+				} else {
+					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("itemsmp:shorkiesqueek")), SoundSource.MASTER, 1, 1, false);
+				}
 			}
 		}
 	}
